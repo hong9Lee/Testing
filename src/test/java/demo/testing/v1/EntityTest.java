6133f28b7b8a7b6d43f4cc26a9d29714058e5485
hong9Lee <hongtemp9mac@gmail.com>
2023-04-15 14:51:02 +0900

    init

---
+++ src/test/java/demo/testing/v1/EntityTest.java
@@ -0,0 +1,14 @@
+package demo.testing.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EntityTest {
+
+    @Test
+    void entityTest() {
+    }
+
+
+}
